a1a910d6f758157ecd1035a033077db60f4c7783
Ryan <park784612@gmail.com>
2026-02-12 18:40:53 +0900

    Create gyeonggi_analysis.sql

---
+++ .sql/gyeonggi_analysis.sql
@@ -0,0 +1,32 @@
+--analysis queries are here
+
+--With this query, I could find out the top 5 city that have high population and among that cities, have low density.
+SELECT
+	region,
+	pop_2059,
+	num_academy,
+	aca_den_10k
+from gyeonggi_en_density_pop
+order by pop_2059 desc, aca_den_10k ASC
+limit 5;
+
+
+--find the average. After then, 
+with avg_density as (
+	SELECT
+		round(avg(aca_den_10k),2) as average_density
+	from gyeonggi_en_density_pop
+)
+SELECT
+	g.region,
+	g.pop_2059,
+	g.num_academy,
+	g.aca_den_10k,
+	round(a.average_density,2) as avg_den_10k,
+	round(a.average_density - g.aca_den_10k,2) as density_gap,
+	round(g.pop_2059 * (a.average_density - g.aca_den_10k) / 10000.0 ,2) as opportunity_score
+from gyeonggi_en_density_pop g
+cross join avg_density as a
+where a.average_density > g.aca_den_10k
+order by g.pop_2059 DESC
+limit 10;
